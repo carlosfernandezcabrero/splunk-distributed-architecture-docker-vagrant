eec8a060cf8a8232dad34ce005a7aa3f0e8a79d5
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-11-15 14:58:28 +0100

    Simplify shcluster member creation

---
+++ splunk-enterprise/configs/shc-member/bootstrap_captain.py
@@ -0,0 +1,16 @@
+import os
+
+file = open("shcluster_members.txt", "r")
+contents = file.read().splitlines()
+file.close()
+
+server_list = ":8089,https://".join(contents)
+
+cmd_start_splunk = (
+    "/usr/local/splunk/bin/splunk start --answer-yes --accept-license --no-prompt"
+)
+cmd_bootstrap_captain = '/usr/local/splunk/bin/splunk bootstrap shcluster-captain -servers_list "https://{}:8089" -auth admin:admin1234'.format(
+    server_list
+)
+
+os.system(" && ".join([cmd_start_splunk, cmd_bootstrap_captain]))
