1e342101d6e4c33835ac6ac0c6acc91d208175bb
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-11-16 13:06:22 +0100

    Update server configuration path and add default value

--- common/configs/server.py
+++ common/configs/server.py
@@ -3,15 +3,17 @@
 import sys
 from configparser import ConfigParser
 
+server_path = sys.argv[2] if len(sys.argv) > 2 else "/tmp/server.conf"
+
 config = ConfigParser()
 config.optionxform = str
 
-config.read("/tmp/server.conf")
+config.read(server_path)
 
 if not config.has_section("general"):
     config.add_section("general")
 
 config["general"]["serverName"] = sys.argv[1]
 
-with open("/tmp/server.conf", "w") as config_file:
+with open(server_path, "w") as config_file:
     config.write(config_file)
