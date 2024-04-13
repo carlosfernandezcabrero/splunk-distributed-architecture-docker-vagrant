17b506e91292820fb417e7fcef0714a0df63e363
Carlos Fernandez Cabrero <56631428+carlosfernandezcabrero@users.noreply.github.com>
2024-04-13 21:04:07 +0200

    Add default command to env variables CONTAINER_RUN and CONTAINER_RUN_HF

--- splunk-enterprise/env.example.rb
+++ splunk-enterprise/env.example.rb
@@ -1,4 +1,4 @@
 ENV['RELEASE_URL'] = ""
 ENV['DOCKER_FROM'] = ""
-ENV['CONTAINER_RUN'] = ""
-ENV['CONTAINER_RUN_HF'] = ""
+ENV['CONTAINER_RUN'] = "echo 'No container run command specified'"
+ENV['CONTAINER_RUN_HF'] = "echo 'No container run command specified'"
