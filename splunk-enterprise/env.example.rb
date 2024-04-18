835db6bbe63e776ba919c60dc3b81d85617f36e9
Carlos Fernandez Cabrero <56631428+carlosfernandezcabrero@users.noreply.github.com>
2024-04-18 08:44:30 +0200

    Parametrize IP base of manager and heavy forwarder

--- splunk-enterprise/env.example.rb
+++ splunk-enterprise/env.example.rb
@@ -1,3 +1,4 @@
 ENV['DOCKER_FROM'] = ""
 ENV['CONTAINER_RUN'] = "echo No container run command specified"
 ENV['CONTAINER_RUN_HF'] = "echo No container run command specified"
+ENV['BASE_IP'] = "xxx.xxx.xxx"
