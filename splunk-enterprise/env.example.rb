9b223ebe14b76984fb25f4343ae1acb03db5533f
Carlos Fernandez Cabrero <56631428+carlosfernandezcabrero@users.noreply.github.com>
2024-04-19 09:09:29 +0200

    Update env.example.rb to remove RELEASE_URL variable

--- splunk-enterprise/env.example.rb
+++ splunk-enterprise/env.example.rb
@@ -1,4 +1,3 @@
-ENV['RELEASE_URL'] = ""
 ENV['DOCKER_FROM'] = ""
 ENV['CONTAINER_RUN'] = "echo No container run command specified"
 ENV['CONTAINER_RUN_HF'] = "echo No container run command specified"
